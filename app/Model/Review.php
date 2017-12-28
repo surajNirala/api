1b86e7ac819ef657ae90369089a8d75f7f7044e3
surajNirala <suraj.nirala1995@gmail.com>
2017-12-28 18:54:03 +0530

    Relationship create

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function products()
+    {
+    	return $this->belongsTo('App\Model\Product','id');
+
+    }
 }

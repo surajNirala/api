98ab89f3266d81505b5a44e6d4e7869b98642c57
surajNirala <suraj.nirala1995@gmail.com>
2017-12-28 15:28:14 +0530

    Migration created for products and reviews

--- database/migrations/2017_12_28_091235_create_products_table.php
+++ database/migrations/2017_12_28_091235_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->text('detail');
+            $table->integer('stock');
+            $table->integer('discount');
             $table->timestamps();
         });
     }
